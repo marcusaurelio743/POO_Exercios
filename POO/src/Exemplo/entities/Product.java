739e4d9034ff7a060014010d96232fd3e2f197ff
marcus aurelio <marcusaurelio743@gmail.com>
2024-06-28 10:27:59 -0300

    usando o encapsulamento no java

--- POO/src/Exemplo/entities/Product.java
+++ POO/src/Exemplo/entities/Product.java
@@ -1,9 +1,9 @@
 package Exemplo.entities;
 
 public class Product {
-	public String name;
-	public Double price;
-	public int quantity;
+	private String name;
+	private Double price;
+	private int quantity;
 	
 	public Product() {
 		
@@ -21,6 +21,30 @@ public class Product {
 		
 	}
 	
+	
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	
+
 	public double totalValueInStock() {
 		return price * quantity;
 	}
